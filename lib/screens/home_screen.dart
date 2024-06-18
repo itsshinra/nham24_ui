0ce76092cc792bbda76eee5c86ecf4118b588f73
sovanmakara-test <sovanmakara@gmail.com>
2024-06-19 00:48:17 +0700

    finish home screen but not implement image and data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,8 +27,9 @@ class _HomeScreenState extends State<HomeScreen> {
               _buildGiftVoucher(),
               const SizedBox(height: 24),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
+                padding: const EdgeInsets.only(left: 16),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text(
                       'TRENDING NOW',
@@ -38,12 +39,135 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ),
                     const SizedBox(height: 16),
+                    _buildTrendingNowItem(),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                child: ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return _buildNham24Item();
+                  },
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "WHAT'S NEW?",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    _buildWhatNew(),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                child: GridView(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1 / 0.5,
+                  ),
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  children: [
                     Container(
-                      height: 130,
-                      width: 130,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 8),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      height: 80,
+                      width: MediaQuery.sizeOf(context).width * 0.5,
                       decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(10),
+                        color: nContainerColor,
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Expanded(child: Text('CONTACT US')),
+                          Image.network(
+                            'https://cdn-icons-png.flaticon.com/128/4370/4370113.png',
+                            width: 40,
+                            height: 40,
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 8),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      height: 80,
+                      width: MediaQuery.sizeOf(context).width * 0.5,
+                      decoration: BoxDecoration(
+                        color: nContainerColor,
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Expanded(child: Text('START\nSELLING')),
+                          Image.network(
+                            'https://cdn-icons-png.flaticon.com/128/9298/9298113.png',
+                            width: 40,
+                            height: 40,
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 8),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      height: 80,
+                      width: MediaQuery.sizeOf(context).width * 0.5,
+                      decoration: BoxDecoration(
+                        color: nContainerColor,
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Expanded(child: Text('BECOME\nRIDER')),
+                          Image.network(
+                            'https://cdn-icons-png.flaticon.com/128/5457/5457799.png',
+                            width: 40,
+                            height: 40,
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 8),
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      height: 80,
+                      width: MediaQuery.sizeOf(context).width * 0.5,
+                      decoration: BoxDecoration(
+                        color: nContainerColor,
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Expanded(child: Text('RATE US')),
+                          Image.network(
+                            'https://cdn-icons-png.flaticon.com/128/1365/1365358.png',
+                            width: 40,
+                            height: 40,
+                          )
+                        ],
                       ),
                     ),
                   ],
@@ -52,6 +176,275 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildWhatNew() {
+    return SizedBox(
+      height: 450,
+      child: GridView(
+        scrollDirection: Axis.horizontal,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 0,
+          mainAxisSpacing: 16,
+          childAspectRatio: 1 / 0.8,
+        ),
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 185,
+                width: 185,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('Shop new delivery now!'),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 185,
+                width: 185,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('Shop new delivery now!'),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 185,
+                width: 185,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('Shop new delivery now!'),
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 185,
+                width: 185,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(10),
+                  image: const DecorationImage(
+                    image: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('Shop new delivery now!'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNham24Item() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'NHAM24 Fresh | Open 24/7',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Container(
+            height: 210,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(15),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://www.wooribank.com.kh/wp-content/uploads/2022/11/Nham24-Web-1.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            'NHAM24 Fresh | Open 24/7',
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTrendingNowItem() {
+    return SizedBox(
+      height: 330,
+      child: GridView(
+        scrollDirection: Axis.horizontal,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+        children: [
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            height: 135,
+            width: 135,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(10),
+              image: const DecorationImage(
+                image: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgtX_uCQ_k0BvIAqyYcdrznL0fuD8-0HyVXZYkASwWbm8q5iq-5oQiFDEJcq0xWVqUoRE&usqp=CAU'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -135,7 +528,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildCategory(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.sizeOf(context).height * 0.26,
+      height: 250,
       child: GridView(
         physics: const NeverScrollableScrollPhysics(),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -281,7 +674,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _buildBannerPromotion() {
     return SizedBox(
-      height: MediaQuery.sizeOf(context).height * 0.2,
+      height: 200,
       child: PageView.builder(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
@@ -292,10 +685,11 @@ class _HomeScreenState extends State<HomeScreen> {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
               image: const DecorationImage(
-                  image: NetworkImage(
-                    'https://cdn6.aptoide.com/imgs/d/2/2/d22aaee0212a689cf5a9e5564baa04ca_fgraphic.jpg',
-                  ),
-                  fit: BoxFit.cover),
+                image: NetworkImage(
+                  'https://cdn6.aptoide.com/imgs/d/2/2/d22aaee0212a689cf5a9e5564baa04ca_fgraphic.jpg',
+                ),
+                fit: BoxFit.cover,
+              ),
             ),
           );
         },
